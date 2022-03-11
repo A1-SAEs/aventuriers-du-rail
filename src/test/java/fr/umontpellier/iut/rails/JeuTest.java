a94758232823214105b1b0ecc41d6f299fed159f
Odiardo Lois <lois.odiardo@etu.umontpellier.fr>
2022-03-11 12:02:45 +0100

    retirerCarteWagonVisible et piocherDestination corrigés

--- src/test/java/fr/umontpellier/iut/rails/JeuTest.java
+++ src/test/java/fr/umontpellier/iut/rails/JeuTest.java
@@ -1,12 +1,26 @@
 package fr.umontpellier.iut.rails;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 class JeuTest {
 
+    List<CouleurWagon> pileCarteWagon= new ArrayList<>();
 
+    @BeforeEach
+    void setUp() {
+        for(int i=0;i<14;i++){
+            pileCarteWagon.add(CouleurWagon.LOCOMOTIVE);
+        }
+        for(int i=0;i<12;i++){
+            pileCarteWagon.addAll(CouleurWagon.getCouleursSimples());
+        }
+    }
 
     @Test
     public void test_defausser_carte_wagon_ajouter_carte_dans_defausse(){
