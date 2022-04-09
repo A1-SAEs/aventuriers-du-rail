f524f9234937319368da853b16610033a628baf1
renuccis <stella-maria.renucci@etu.umontpellier.fr>
2022-04-09 12:15:18 +0200

    Réglages des tests étudiants avec les méthodes mises à jour

--- src/test/java/fr/umontpellier/iut/rails/JeuEtudiantTest.java
+++ src/test/java/fr/umontpellier/iut/rails/JeuEtudiantTest.java
@@ -2,39 +2,21 @@ package fr.umontpellier.iut.rails;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.beans.DesignMode;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-class JeuTest {
+class JeuEtudiantTest {
     Jeu Partie = new Jeu(new String[]{"Lolo", "Stella"});
     List<CouleurWagon> pileCartesWagon = Partie.getPileCartesWagon();
     List<CouleurWagon> defausseCartesWagon = Partie.getDefausseCartesWagon();
     List<CouleurWagon> cartesWagonVisibles = Partie.getCartesWagonVisibles();
     List<Destination> pileDestinations = Partie.getPileDestinations();
-
-    @BeforeEach
-    void setUp() {
-        //Mise en place de la pile des wagons : 110 cartes
-        for (int i = 0; i < 14; i++) {
-            pileCartesWagon.add(CouleurWagon.LOCOMOTIVE);
-        }
-        for (int i = 0; i < 12; i++) {
-            pileCartesWagon.addAll(CouleurWagon.getCouleursSimples());
-        }
-        Collections.shuffle(pileCartesWagon);
-
-        //Mise en place de la pile des destinations : 46 cartes
-        pileDestinations.addAll(Destination.makeDestinationsEurope());
-    }
+    //Nombre cartes après distrib aux joueurs : 102
+    //Nombre cartes après distrib cartes visibles : 97
 
     /////defausserCarteWagon/////
-    @Disabled //Fonctionne
+     //Fonctionne
     @Test
     public void test_ajouter_carte_dans_defausse_vide() {
         Partie.defausserCarteWagon(CouleurWagon.ROUGE);
@@ -42,18 +24,7 @@ class JeuTest {
         assertEquals(1, defausseCartesWagon.size());
     }
 
-    @Disabled //Fonctionne
-    @Test
-    public void test_ajouter_carte_dans_cartes_visibles() {
-        pileCartesWagon.clear();
-        defausseCartesWagon.clear();
-        cartesWagonVisibles.add(CouleurWagon.ROSE);
-        Partie.defausserCarteWagon(CouleurWagon.ROUGE);
-        System.out.println(cartesWagonVisibles);
-        assertEquals(2, cartesWagonVisibles.size());
-    }
-
-    @Disabled //Fonctionne
+     //Fonctionne
     @Test
     public void test_ne_pas_ajouter_carte_dans_cartes_visibles_car_egale_a_5_malgre_pile_et_defausse_vides() {
         pileCartesWagon.clear();
@@ -68,24 +39,25 @@ class JeuTest {
     }
 
     /////piocherCarteWagon/////
-    @Disabled //Fonctionne
+    //Fonctionne
     @Test
     public void test_pile_et_defausse_remplies() {
         Partie.piocherCarteWagon();
         System.out.println(pileCartesWagon);
-        assertEquals(109, pileCartesWagon.size());
+
+        assertEquals(96, pileCartesWagon.size());
     }
 
-    @Disabled //Fonctionne
+    //Fonctionne
     @Test
     public void test_pile_vide_et_defausse_remplie() {
         defausseCartesWagon.addAll(pileCartesWagon);
         pileCartesWagon.clear();
         Partie.piocherCarteWagon();
-        assertEquals(109, pileCartesWagon.size());
+        assertEquals(97, pileCartesWagon.size());
     }
 
-    @Disabled //Fonctionne
+    //Fonctionne
     @Test
     public void test_pile_et_defausse_vides() {
         pileCartesWagon.clear();
@@ -94,18 +66,15 @@ class JeuTest {
     }
 
     /////retirerCarteWagonVisible/////
-    @Disabled //Fonctionne
+    //Fonctionne
     @Test
     public void test_piocher_carte_wagon_visible() {
-        for (int i = 0; i < 5; i++) {
-            cartesWagonVisibles.add(CouleurWagon.ROUGE);
-        }
-        Partie.retirerCarteWagonVisible(CouleurWagon.ROUGE);
+        Partie.retirerCarteWagonVisible(cartesWagonVisibles.get(0));
         System.out.println(cartesWagonVisibles);
         assertEquals(5, cartesWagonVisibles.size());
     }
 
-    //A refaire/revoir
+    //Fonctionne
     @Test
     public void test_3_locomotives() {
         cartesWagonVisibles.clear();
@@ -121,14 +90,14 @@ class JeuTest {
     }
 
     /////piocherDestination/////
-    @Disabled //Fonctionne
+    //Fonctionne
     @Test
     public void test_pile_destination_remplie() {
         Partie.piocherDestination();
-        assertEquals(45, pileDestinations.size());
+        assertEquals(39, pileDestinations.size());
     }
 
-    @Disabled //Fonctionne
+    //Fonctionne
     @Test
     public void test_pile_destination_vide() {
         pileDestinations.clear();
